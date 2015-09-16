ddbf25cdded5d7a08306299b8b607efcb635d1e7
Scott <scottbot95@gmail.com>
2015-09-15 23:34:30 -0700

    Created pins.h file

---
+++ RautekCoilgunMk2_MCU/src/pins.h
@@ -0,0 +1,15 @@
+/*
+ * pins.h
+ *
+ * Contains a bunch of defines for pin numbers
+ *
+ *  Created on: Sep 15, 2015
+ *      Author: scott
+ */
+
+#ifndef PINS_H_
+#define PINS_H_
+
+
+
+#endif /* PINS_H_ */
