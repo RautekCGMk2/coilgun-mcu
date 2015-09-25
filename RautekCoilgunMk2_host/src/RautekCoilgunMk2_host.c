988fe0eb72d20df98eb6e9a552112e95d2e4df41
scott <scottbot95@gmail.com>
2015-09-25 01:14:20 -0700

    Changed buid dialet to c11

--- RautekCoilgunMk2_host/src/RautekCoilgunMk2_host.c
+++ RautekCoilgunMk2_host/src/RautekCoilgunMk2_host.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <syslog.h>
 
 int main() {
-  /* Setup your example here, code that should run once
-   */
+	/* Setup your example here, code that should run once
+	 */
+	openlog("RTCGMk2", LOG_PID | LOG_CONS, LOG_USER);
 
+	/* Code in this loop will run repeatedly
+	 */
+	for (;;) {
 
-  /* Code in this loop will run repeatedly
-   */
-  for (;;) {
+	}
 
-  }
-
-  return 0;
+	return 0;
 }
